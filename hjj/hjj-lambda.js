770dc553338f383163fdeae88907dcacd5a1a3e7
thuvva <35223608+thuvva@users.noreply.github.com>
2018-01-16 14:34:19 +0530

    3rd

--- hjj/hjj-lambda.js
+++ hjj/hjj-lambda.js
@@ -3,7 +3,7 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
 	sns.unsubscribe({
-		SubscriptionArn: 'arn:aws:sns:us-east-1:480964559519:mockT1:arn:aws:sns:us-east-1:480964559519:mockT1:a17b5ada-c672-432b-bb1d-f384a3fe289b'
+		SubscriptionArn: 'arn:aws:sns:us-east-1:480964559519:mockT1:a17b5ada-c672-432b-bb1d-f384a3fe289b'
 	}).promise()
 		.then(data => {
 			console.log(data);
